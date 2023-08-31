a517e36c21fc5ffc7efd7fae1760ce38695ca85e
Sanshai <vbn-134@yandex.ru>
2023-08-31 16:04:46 +0300

    корректировка стилей

--- src/js/vidget.js
+++ src/js/vidget.js
@@ -1,51 +1,55 @@
 export default class Vidget {
   constructor(validCard) {
     this.validCard = validCard;
-    this.input = document.querySelector('.input')
-    this.conteiner = document.querySelector('.box-conteiner')
+    this.input = document.querySelector(".input");
+    this.conteiner = document.querySelector(".box-conteiner");
   }
 
-
-
   vidget(bool) {
-    return bool ? this.cardNumberTrue() : this.cardNumberFalse()
+    return bool ? this.cardNumberTrue() : this.cardNumberFalse();
   }
 
   cardNumberTrue() {
-    const div = document.createElement('div');
-    div.classList.add('valid')
+    const div = document.createElement("div");
+    div.classList.add("valid");
     div.innerHTML = `
       <div class="valid-true">
         <h2>Card is valid</h2>
-      </div>`
-    this.conteiner.appendChild(div)
+      </div>`;
+    this.conteiner.appendChild(div);
+    this.input.classList.add("card-valid");
+
     setInterval(() => {
       this.conteiner.removeChild(div);
-      this.input.value = ''
+      this.input.value = "";
+      this.input.classList.remove('card-valid')
       document.querySelectorAll(".card").forEach((evt) => {
         evt.classList.remove("active");
       });
-    }, 3000)
+    }, 3000);
   }
 
   cardNumberFalse() {
     document.querySelectorAll(".card").forEach((evt) => {
       evt.classList.remove("active");
     });
-    const div = document.createElement('div');
-    div.classList.add('valid')
+    this.input.classList.add("card-invalid");
+    const div = document.createElement("div");
+    div.classList.add("valid");
     div.innerHTML = `
       <div class="valid-false">
         <h2>Card is not valid</h2>
-      </div>`
+      </div>`;
+    this.conteiner.appendChild(div);
+    this.input.classList.add("card-invalid");
 
-    this.conteiner.appendChild(div)
     setInterval(() => {
       this.conteiner.removeChild(div);
-      this.input.value = ''
+      this.input.value = "";
+      this.input.classList.remove("card-invalid");
       document.querySelectorAll(".card").forEach((evt) => {
         evt.classList.remove("active");
       });
-    }, 3000)
+    }, 3000);
   }
 }
